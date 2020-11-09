70022d20af2ef4d67c1ff619251d3f77b6b0b442
Paul Cailly <pcailly@pm.me>
2020-11-09 20:39:55 +0100

    build(nextjs): update next.config.js

--- next.config.js
+++ next.config.js
@@ -21,11 +21,8 @@ module.exports = withBundleAnalyzer(
 
             config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
             config.resolve.alias['@hooks'] = path.join(__dirname, 'src/hooks');
-            config.resolve.alias['@middlewares'] = path.join(__dirname, 'src/middlewares');
             config.resolve.alias['@pages'] = path.join(__dirname, 'src/pages');
-            config.resolve.alias['@stores'] = path.join(__dirname, 'src/stores');
             config.resolve.alias['@styles'] = path.join(__dirname, 'src/styles');
-            config.resolve.alias['@typings'] = path.join(__dirname, 'src/typings');
             config.resolve.alias['@utils'] = path.join(__dirname, 'src/utils');
 
             return config;
