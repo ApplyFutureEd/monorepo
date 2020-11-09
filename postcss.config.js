3a484cf1d68a2f4ad3eeab6fa640444b5fed12d0
Paul Cailly <pcailly@pm.me>
2020-11-09 22:14:52 +0100

    fix(tailwindcss): fix purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,16 @@
 module.exports = {
     plugins: [
         'tailwindcss',
-        'postcss-flexbugs-fixes',
+        'postcss-custom-properties',
+        'autoprefixer',
         [
-            'postcss-preset-env',
+            '@fullhuman/postcss-purgecss',
             {
-                autoprefixer: {
-                    flexbox: 'no-2009'
-                },
-                stage: 3,
-                features: {
-                    'custom-properties': false
-                }
+                content: [
+                    './src/pages/**/*.{js,jsx,ts,tsx}',
+                    './src/components/**/*.{js,jsx,ts,tsx}'
+                ],
+                defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || []
             }
         ]
     ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
+    purge: [],
     theme: {
         container: {
             center: true,
