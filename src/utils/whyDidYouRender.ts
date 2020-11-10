9cecd4026a695595645bb32d43ef2e42da7c0570
Paul Cailly <pcailly@pm.me>
2020-11-10 13:37:56 +0100

    fix(why-did-you-render): grammar

--- src/utils/whyDidYouRender.ts
+++ src/utils/whyDidYouRender.ts
@@ -1,18 +1,18 @@
 import React from 'react';
 
 /**
- * WDYR (why-did-you-render) helps locate unnecessary re-renders and fix them
- * Applied in development environment, on the frontend only
+ * WhyDidYouRender helps locate unnecessary re-renders and fix them
+ * Applied only in front-end development environment
  *
  * @see https://github.com/welldone-software/why-did-you-render
  */
-export const whyDidYouRender = (): void => {
+export const initWhyDidYouRender = (): void => {
     if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         const whyDidYouRender = require('@welldone-software/why-did-you-render');
         // eslint-disable-next-line no-console
         console.debug(
-            'Applying whyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render'
+            'Initializing WhyDidYouRender, to help you locate unnecessary re-renders during development. See https://github.com/welldone-software/why-did-you-render'
         );
         whyDidYouRender(React, {
             trackAllPureComponents: true,
