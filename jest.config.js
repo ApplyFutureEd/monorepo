6db8c016e9e61142688adea0c98f853eb1451d39
Paul Cailly <pcailly@pm.me>
2020-11-12 01:06:54 +0100

    fix(formik): add missing deps

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-    testResultsProcessor: 'jest-sonar-reported',
+    testResultsProcessor: 'jest-sonar-reporter',
     transform: {
         '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
     },
