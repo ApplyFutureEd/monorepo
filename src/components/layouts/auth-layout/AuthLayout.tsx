da5db9b9e7e66ef3d89065fd031dc21c26a08e50
Paul Cailly <pcailly@pm.me>
2020-11-17 02:04:09 +0100

    test(sign-in): add tests

--- src/components/layouts/auth-layout/AuthLayout.tsx
+++ src/components/layouts/auth-layout/AuthLayout.tsx
@@ -1,25 +1,30 @@
+import Head from '@components/core/head/Head';
 import { FC, ReactNode } from 'react';
 
 type Props = {
     children: ReactNode;
+    title: string;
 };
 
 const AuthLayout: FC<Props> = (props) => {
-    const { children } = props;
+    const { children, title } = props;
 
     return (
-        <div className="flex min-h-screen bg-white">
-            <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-                <div className="mx-auto w-full max-w-sm lg:w-96">{children}</div>
+        <>
+            <Head title={title} />
+            <div className="flex min-h-screen bg-white">
+                <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+                    <div className="mx-auto w-full max-w-sm lg:w-96">{children}</div>
+                </div>
+                <div className="relative hidden flex-1 w-0 lg:block">
+                    <img
+                        alt=""
+                        className="absolute inset-0 w-full h-full object-contain"
+                        src="/assets/images/landing/auth.svg"
+                    />
+                </div>
             </div>
-            <div className="relative hidden flex-1 w-0 lg:block">
-                <img
-                    alt=""
-                    className="absolute inset-0 w-full h-full object-contain"
-                    src="/assets/images/landing/auth.svg"
-                />
-            </div>
-        </div>
+        </>
     );
 };
 
