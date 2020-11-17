0d277cc72f1d1c92ceae4d567de152322cce329e
Paul Cailly <pcailly@pm.me>
2020-11-17 09:55:26 +0100

    feat(sign-up): add sign-up page

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,6 @@
+import AuthLayout from '@components/layouts/auth-layout/AuthLayout';
+import { FC } from 'react';
+
+const SignInPage: FC = () => <AuthLayout title="Sign up">Hello World</AuthLayout>;
+
+export default SignInPage;
