c920e50d3c7698e1fbe366e6dd936abbbff517c2
Paul Cailly <pcailly@pm.me>
2020-11-17 02:07:57 +0100

    fix(sign-in page): add missing props

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -3,7 +3,7 @@ import AuthLayout from '@components/layouts/auth-layout/AuthLayout';
 import { FC } from 'react';
 
 const SignInPage: FC = () => (
-    <AuthLayout>
+    <AuthLayout title="Sign in">
         <SignIn />
     </AuthLayout>
 );
