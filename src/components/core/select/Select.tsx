c69583d9a3f3a350cfed06deae478d3c36e395d7
Paul Cailly <pcailly@pm.me>
2020-11-19 02:03:16 +0100

    feat(select): add Select component

---
+++ src/components/core/select/Select.tsx
@@ -0,0 +1,112 @@
+import Tooltip from '@components/core/tooltip/Tooltip';
+import cx from 'classnames';
+import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import Skeleton from 'react-loading-skeleton';
+import Select from 'react-select';
+
+type Props = {
+    disabled: boolean;
+    field: any;
+    isLoading?: boolean;
+    label: string;
+    meta: any;
+    options: Array<{ value: string; label: string }>;
+    optional?: boolean;
+    setFieldValue: any;
+    tooltip?: string;
+};
+
+const SelectMulti: FC<Props> = (props) => {
+    const {
+        disabled,
+        field,
+        isLoading = false,
+        label,
+        meta,
+        options,
+        optional,
+        setFieldValue,
+        tooltip = '',
+        ...rest
+    } = props;
+
+    const { t } = useTranslation(['common']);
+    const onError = Boolean(meta?.touched && meta?.error);
+
+    const baseClasses =
+        'block form-select w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5';
+    const disabledClasses = 'bg-gray-100 cursor-not-allowed';
+    const onErrorClasses =
+        'pr-10 border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red';
+
+    const classes = cx({
+        [`${baseClasses}`]: true,
+        [`${disabledClasses}`]: disabled,
+        [`${onErrorClasses}`]: onError
+    });
+
+    if (isLoading) {
+        return (
+            <div>
+                <div className="block text-gray-700 text-sm font-medium leading-5">
+                    <Skeleton height="16px" width="120px" />
+                </div>
+                <div className="relative rounded-md shadow-sm">
+                    <Skeleton height="40px" width="100%" />
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <label className="block font-sans" htmlFor={field?.name} {...rest}>
+            <Tooltip content={tooltip}>
+                <div>
+                    <span className="text-gray-700 text-sm font-medium leading-5">{label}</span>
+                    {optional && (
+                        <span className="text-gray-500 text-sm uppercase">
+                            {' '}
+                            - {t('common:optional')}
+                        </span>
+                    )}
+                </div>
+            </Tooltip>
+
+            <div className="mt-1">
+                <div className="rounded-md shadow-sm">
+                    <Select
+                        isMulti
+                        className={classes}
+                        inputId={field.name}
+                        name={field.name}
+                        options={options}
+                        placeholder=""
+                        value={
+                            (options
+                                ? options.filter((option) => field.value.includes(option.value))
+                                : '') as any
+                        }
+                        onChange={(options: any) => {
+                            if (!options) {
+                                return setFieldValue(field.name, '');
+                            }
+
+                            return setFieldValue(
+                                field.name,
+                                options.map((option: any) => option.value)
+                            );
+                        }}
+                    />
+                </div>
+            </div>
+            {onError && (
+                <p className="mt-2 text-red-600 text-sm" id={`${field?.name}-error`}>
+                    {meta?.error}
+                </p>
+            )}
+        </label>
+    );
+};
+
+export default SelectMulti;
