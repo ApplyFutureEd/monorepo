4b0aeb1919d866ca13f0c74d9faedc10f68cd992
Paul Cailly <pcailly@pm.me>
2020-12-06 23:21:28 +0100

    fix(terms and conditions): fix css not being applied to document

--- src/components/landing/root/terms-and-conditions/TermsAndConditions.tsx
+++ src/components/landing/root/terms-and-conditions/TermsAndConditions.tsx
@@ -1,12 +1,13 @@
 import Document from '@docs/terms-and-conditions.md';
-import React, { FC } from 'react';
+import { FC } from 'react';
 
 const TermsAndConditions: FC = () => {
     return (
         <div className="mx-auto py-0 max-w-7xl sm:px-6 md:py-6 lg:px-8">
             <div className="doc px-4 sm:px-0">
-                <div className="markdown" />
-                <Document />
+                <div className="markdown">
+                    <Document />
+                </div>
             </div>
         </div>
     );
