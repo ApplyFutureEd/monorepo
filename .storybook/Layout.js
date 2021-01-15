c7576ccfb094ca1b138f0adf59cf5657ce95be50
Paul Cailly <pcailly@pm.me>
2021-01-15 12:30:35 +0100

    fix(storybook): fix assets path

--- .storybook/Layout.js
+++ .storybook/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './packages/students/src/styles/index.css';
-import './packages/students/public/assets/fonts/inter/font.css';
+import './../packages/students/src/styles/index.css';
+import './../packages/students/public/assets/fonts/inter/font.css';
 
 const Layout = ({ children }) => {
     return <div>{children}</div>;
