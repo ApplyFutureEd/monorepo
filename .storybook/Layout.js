b510700cec09428be8c9686179d1eed7f5b19ee6
Paul Cailly <pcailly@pm.me>
2021-01-15 19:12:13 +0100

    fix(storybook): remove storybook

--- .storybook/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './../packages/students/src/styles/index.css';
-import './../packages/students/public/assets/fonts/inter/font.css';
-
-const Layout = ({ children }) => {
-    return <div>{children}</div>;
-};
-
-export default Layout;
--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    addons: ['@storybook/addon-essentials', 'storybook-addon-designs'],
-    presets: [path.resolve(__dirname, './next-preset.js')],
-    stories: ['../**/*.story.tsx']
-};
