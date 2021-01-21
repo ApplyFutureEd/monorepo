cc67bd2691350f8188ab3138bce3734adc598fc5
Paul Cailly <pcailly@pm.me>
2021-01-21 16:33:19 +0100

    refactor(programs): split components

---
+++ packages/admin/src/components/tables/programs/Table.tsx
@@ -0,0 +1,187 @@
+import {
+    SearchableProgramFilterInput,
+    SearchableProgramSortableFields,
+    SearchableProgramSortInput,
+    SearchableSortDirection,
+    SearchProgramsQuery,
+    SearchProgramsQueryVariables
+} from '@applyfuture/graphql';
+import { Program } from '@applyfuture/models';
+import DateFormatter from '@components/tables/common/DateFormatter';
+import ResizingPanel from '@components/tables/common/ResizingPanel';
+import TableRow from '@components/tables/common/TableRow';
+import {
+    DataTypeProvider,
+    Filter,
+    FilteringState,
+    SearchState,
+    Sorting,
+    SortingState,
+    TableColumnWidthInfo
+} from '@devexpress/dx-react-grid';
+import {
+    Grid,
+    SearchPanel,
+    TableColumnResizing,
+    TableFilterRow,
+    TableHeaderRow,
+    Toolbar,
+    VirtualTable
+} from '@devexpress/dx-react-grid-material-ui';
+import min from 'date-fns/min';
+import React, { FC, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+
+type Props = {
+    data: SearchProgramsQuery;
+    handleContextMenu: (event: React.MouseEvent, row: Program) => void;
+    setVariables: React.Dispatch<React.SetStateAction<SearchProgramsQueryVariables>>;
+};
+
+const Table: FC<Props> = (props) => {
+    const { data, handleContextMenu, setVariables } = props;
+
+    const [columns] = useState([
+        { name: 'updatedAt', title: 'Last update' },
+        { name: 'name', title: 'Name' },
+        {
+            getCellValue: (row: Program) => row.school?.name,
+            name: 'schoolName',
+            title: 'School'
+        },
+        {
+            name: 'city',
+            title: 'City'
+        },
+        {
+            getCellValue: (row: Program) => (row.published ? 'Published' : 'Unlisted'),
+            name: 'published',
+            title: 'Published'
+        },
+        { name: 'degree', title: 'Degree' },
+        {
+            getCellValue: (row: Program) =>
+                min(row.intakes.split(',').map((intake: string) => new Date(intake))),
+            name: 'intakes',
+            title: 'Next Intake'
+        },
+        {
+            getCellValue: (row: Program) => row.submissionDeadline,
+            name: 'submissionDeadline',
+            title: 'Submission Deadline'
+        }
+    ]);
+    const [defaultColumnWidths] = useState([
+        { columnName: 'updatedAt', width: 140 },
+        { columnName: 'name', width: 440 },
+        { columnName: 'schoolName', width: 180 },
+        { columnName: 'city', width: 80 },
+        { columnName: 'published', width: 80 },
+        { columnName: 'degree', width: 80 },
+        { columnName: 'intakes', width: 80 },
+        { columnName: 'submissionDeadline', width: 80 }
+    ]);
+    const [columnWidths, setColumnWidths] = useState<TableColumnWidthInfo[]>(defaultColumnWidths);
+    const [resizingMode, setResizingMode] = useState('widget');
+    const handleResetWidths = () => {
+        setColumnWidths(defaultColumnWidths);
+    };
+    const handleColumnWidthsChange = (nextColumnWidths: TableColumnWidthInfo[]) => {
+        setColumnWidths(nextColumnWidths);
+    };
+    const handleSearch = (gridSearchValue: string) => {
+        if (!gridSearchValue) {
+            return setVariables({
+                limit: 20
+            });
+        }
+        setVariables({
+            filter: {
+                or: [
+                    { name: { matchPhrasePrefix: gridSearchValue } },
+                    { city: { matchPhrasePrefix: gridSearchValue } },
+                    { schoolName: { matchPhrasePrefix: gridSearchValue } }
+                ]
+            },
+            limit: 20
+        });
+    };
+    const debouncedSearch = useDebouncedCallback((gridSearchValue: string) => {
+        handleSearch(gridSearchValue);
+    }, 2000);
+    const handleFilters = (gridFilters: Filter[]) => {
+        const filter: SearchableProgramFilterInput = {
+            and: gridFilters.map((gridFilter) => ({
+                [`${gridFilter.columnName}`]: { matchPhrasePrefix: gridFilter.value }
+            }))
+        };
+
+        if (filter?.and?.length || 0 > 0) {
+            setVariables((prev: SearchProgramsQueryVariables) => ({
+                ...prev,
+                filter
+            }));
+        } else {
+            setVariables(() => ({
+                limit: 20
+            }));
+        }
+    };
+    const debouncedFilter = useDebouncedCallback((gridFilters: Filter[]) => {
+        handleFilters(gridFilters);
+    }, 2000);
+    const handleSort = (gridSorts: Sorting[]) => {
+        const sort: SearchableProgramSortInput = {
+            direction: gridSorts[0].direction as SearchableSortDirection,
+            field: gridSorts[0].columnName as SearchableProgramSortableFields
+        };
+
+        setVariables((prev: SearchProgramsQueryVariables) => ({
+            ...prev,
+            sort
+        }));
+    };
+
+    return (
+        <Grid columns={columns} rows={data?.searchPrograms?.items || []}>
+            <DataTypeProvider
+                for={['updatedAt']}
+                formatterComponent={(props) => <DateFormatter {...props} scheme="dd/MM/yy HH:MM" />}
+            />
+            <DataTypeProvider
+                for={['intakes', 'submissionDeadline']}
+                formatterComponent={DateFormatter}
+            />
+            <SearchState
+                defaultValue=""
+                onValueChange={(gridSearchValue) => debouncedSearch.callback(gridSearchValue)}
+            />
+            <FilteringState
+                defaultFilters={[]}
+                onFiltersChange={(gridFilters) => debouncedFilter.callback(gridFilters)}
+            />
+            <SortingState defaultSorting={[]} onSortingChange={handleSort} />
+            <VirtualTable
+                rowComponent={(props) => (
+                    <TableRow {...props} handleContextMenu={handleContextMenu} />
+                )}
+            />
+            <TableColumnResizing
+                columnWidths={columnWidths}
+                resizingMode={resizingMode}
+                onColumnWidthsChange={handleColumnWidthsChange}
+            />
+            <TableHeaderRow showSortingControls />
+            <TableFilterRow />
+            <Toolbar />
+            <SearchPanel />
+            <ResizingPanel
+                changeMode={setResizingMode}
+                defaultValue={resizingMode}
+                resetWidths={handleResetWidths}
+            />
+        </Grid>
+    );
+};
+
+export default Table;
