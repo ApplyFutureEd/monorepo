cb0aafb7a67508f783fcc186c10ee715bc03dff1
Paul Cailly <pcailly@pm.me>
2021-04-03 06:43:23 +0200

    fix(careers): fix css

--- packages/students/src/pages_/careers.tsx
+++ packages/students/src/pages_/careers.tsx
@@ -16,16 +16,16 @@ const CareersPage: FC = () => {
     return (
         <LandingLayout description={'Apply for your greatest career'} title={t('Careers')}>
             <main className="lg:relative">
-                <div className="max-w-7xl mx-auto w-full lg:py-24 lg:text-left">
+                <div className="max-w-7xl mx-auto py-8 w-full lg:py-24 lg:text-left">
                     <div className="px-4 space-y-4 sm:px-8">
-                        <h1 className="text-indigo-600 text-xl font-extrabold tracking-tight sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl">
+                        <h1 className="text-indigo-600 text-2xl font-extrabold tracking-tight sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl">
                             An open invitation to open minds
                         </h1>
                         <h2 className="text-gray-900 text-xl font-extrabold tracking-tight sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl">
                             Come to ApplyFuture, where we build the future of foreign education.
                             Here, you’ll do more than join something — you’ll add something.
                         </h2>
-                        <div className="hidden md:block">
+                        <div className="hidden sm:block">
                             <Image
                                 alt="Careers perks at ApplyFuture"
                                 height="480"
@@ -35,8 +35,8 @@ const CareersPage: FC = () => {
                             />
                         </div>
                     </div>
-                    <div className="flex flex-col items-center justify-center mt-16">
-                        <p className="mb-4 max-w-xl text-center text-gray-900 text-xl font-bold tracking-tight sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl">
+                    <div className="items-left flex flex-col justify-center mt-16 px-4 sm:items-center sm:px-8">
+                        <p className="mb-4 max-w-xl text-gray-900 text-xl font-bold tracking-tight sm:text-center sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl">
                             Get discovered. Introduce yourself, and we‘ll get in touch if there‘s a
                             role that seems like a good match.
                         </p>
