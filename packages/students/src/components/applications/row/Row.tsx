26bc4db04468add6d9119d9f92fdac5278316cdf
Paul Cailly <pcailly@pm.me>
2021-05-01 22:44:33 +0200

    fix(application journey documents): remove unused import

--- packages/students/src/components/applications/row/Row.tsx
+++ packages/students/src/components/applications/row/Row.tsx
@@ -4,7 +4,6 @@ import {
     englishTestDocumentsIds,
     frenchTestDocumentsIds,
     logicAndReasoningTestDocumentsIds,
-    logicAndReasoningTests,
     toast
 } from '@applyfuture/utils';
 import { faDownload } from '@fortawesome/pro-light-svg-icons';
