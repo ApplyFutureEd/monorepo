14b413078e7100c40b75f7cba13a6b1644d644fd
Paul Cailly <pcailly@pm.me>
2021-05-12 16:58:36 +0200

    fix(application-update): fix summary

--- packages/utils/src/helpers/scrollToErrors.ts
+++ packages/utils/src/helpers/scrollToErrors.ts
@@ -3,7 +3,6 @@ import { FormikErrors } from 'formik';
 export const scrollToErrors = <FormValues>(errors: FormikErrors<FormValues>): void => {
     try {
         const errorKeys = Object.keys(errors);
-        console.log(errors);
         if (errorKeys.length > 0) {
             document.getElementsByName(errorKeys[0])[0].focus();
         }
