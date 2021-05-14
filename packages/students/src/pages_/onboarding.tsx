008bbf9620d60b3f5a4da4dbb485f21d6ea68335
Yann Rolland <Yann.rolland96@gmail.com>
2021-05-14 17:25:39 +0200

    Revert "feat(onboarding): attempt to revert commit"
    This reverts commit 2448d630348029aee914f76a3d3fbc71fc4bda0c.

--- packages/students/src/pages_/onboarding.tsx
+++ packages/students/src/pages_/onboarding.tsx
@@ -2,7 +2,11 @@ import LandingLayout from '@components/layouts/landing-layout/LandingLayout';
 import { FC } from 'react';
 
 const Onboarding: FC = () => {
-    return <LandingLayout title="onboarding">test</LandingLayout>;
+    return (
+        <LandingLayout title='onboarding'>
+            test
+        </LandingLayout>
+    );
 };
 
 export default Onboarding;
