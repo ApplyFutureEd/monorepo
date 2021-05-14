fb9038324010f2d7263a07a4727b8bd7cf7190ba
Yann Rolland <Yann.rolland96@gmail.com>
2021-05-14 16:54:29 +0200

    feat(onboarding): add onboarding.tsx file, update i18n.js

--- packages/students/src/pages_/onboarding.tsx
+++ packages/students/src/pages_/onboarding.tsx
@@ -1,30 +1,12 @@
-import Arguments from '@components/landing/arguments/Arguments';
-import Contact from '@components/landing/contact/Contact';
-import Hero from '@components/landing/hero/Hero';
-import Partners from '@components/landing/partners/Partners';
-import Process from '@components/landing/process/Process';
-import Recruiters from '@components/landing/recruiters/Recruiters';
-import Schools from '@components/landing/schools/Schools';
-import Students from '@components/landing/students/Students';
 import LandingLayout from '@components/layouts/landing-layout/LandingLayout';
-import useTranslation from 'next-translate/useTranslation';
 import { FC } from 'react';
 
-const LandingPage: FC = () => {
-    const { t } = useTranslation();
-
+const Onboarding: FC = () => {
     return (
-        <LandingLayout description={t('landing:meta-description')} title={t('landing:page-title')}>
-            <Hero />
-            <Students />
-            <Arguments />
-            <Process />
-            <Schools />
-            <Partners />
-            <Recruiters />
-            <Contact />
+        <LandingLayout title='onboarding'>
+            test
         </LandingLayout>
     );
 };
 
-export default LandingPage;
+export default Onboarding;
