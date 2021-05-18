32f268bf9dc4ac474eaea2407a651e9adf86db47
Paul Cailly <pcailly@pm.me>
2021-05-18 20:30:55 +0200

    fix(application-journey): remove unused import

--- packages/students/src/components/programs/row/Row.tsx
+++ packages/students/src/components/programs/row/Row.tsx
@@ -1,8 +1,6 @@
 import {
     createApplication,
     CreateApplicationMutation,
-    getApplicationByStudent,
-    GetApplicationByStudentQuery,
     GetDocumentByStudentQuery,
     GetProgramBySchoolQuery,
     GetStudentByEmailQuery,
