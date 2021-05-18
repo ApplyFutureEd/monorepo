a808d577e3e5800b4dfec4b18988e624be865688
Paul Cailly <pcailly@pm.me>
2021-05-18 03:11:33 +0200

    fix(timeline): fix application content

--- packages/ui/src/timeline/Timeline.tsx
+++ packages/ui/src/timeline/Timeline.tsx
@@ -37,25 +37,25 @@ type Props = {
 };
 
 export const Timeline: FC<Props> = (props) => {
-    const { application, config } = props;
+    const { application: initialApplication, config } = props;
     const router = useRouter();
     const locale = router.locale as SupportedLocale;
     const { t } = useTranslation();
 
-    const [steps, setSteps] = useState(application?.steps);
+    const [application, setApplication] = useState(initialApplication);
     const [item] = useSubscription<any>({
         config: {
             key: 'onUpdateApplicationById',
             query: onUpdateApplicationById,
             variables: {
-                id: application?.id
+                id: initialApplication?.id
             }
         }
     });
 
     useEffect(() => {
-        if (item?.steps) {
-            setSteps(item.steps);
+        if (item?.id) {
+            setApplication(item);
         }
     }, [item]);
 
@@ -63,58 +63,53 @@ export const Timeline: FC<Props> = (props) => {
         <div className="clearfix">
             <ul className="-mb-8">
                 <li>
-                    {application &&
-                        steps &&
-                        steps
-                            .filter((step) => {
-                                if (
-                                    step.id === 'fees-payment' &&
-                                    application.program?.applicationFee &&
-                                    application.program?.applicationFee < 0
-                                ) {
-                                    return false;
-                                }
-                                return true;
-                            })
-                            .map((step) => {
-                                const isLastStep = step.id === 'visa';
+                    {application?.steps
+                        .filter((step) => {
+                            if (
+                                step.id === 'fees-payment' &&
+                                application.program?.applicationFee &&
+                                application.program?.applicationFee < 0
+                            ) {
+                                return false;
+                            }
+                            return true;
+                        })
+                        .map((step) => {
+                            const isLastStep = step.id === 'visa';
 
-                                return (
-                                    <div key={step.id} className="relative pb-8">
-                                        {!isLastStep && (
-                                            <span
-                                                aria-hidden="true"
-                                                className="left-1 top-1 w-0.5 absolute -ml-px h-full bg-gray-200"
-                                            />
-                                        )}
-                                        <div className="relative flex space-x-3">
-                                            <div>
-                                                <StatusIcon status={step.status} />
+                            return (
+                                <div key={step.id} className="relative pb-8">
+                                    {!isLastStep && (
+                                        <span
+                                            aria-hidden="true"
+                                            className="left-1 top-1 w-0.5 absolute -ml-px h-full bg-gray-200"
+                                        />
+                                    )}
+                                    <div className="relative flex space-x-3">
+                                        <div>
+                                            <StatusIcon status={step.status} />
+                                        </div>
+                                        <div className="pt-1.5 flex flex-1 justify-between min-w-0 space-x-4">
+                                            <div className="max-w-xl">
+                                                <p className="text-gray-900 text-sm font-medium">
+                                                    {t(step.timelineLabel)}
+                                                </p>
+                                                {config[step.id][step.status](application, step)}
                                             </div>
-                                            <div className="pt-1.5 flex flex-1 justify-between min-w-0 space-x-4">
-                                                <div className="max-w-xl">
-                                                    <p className="text-gray-900 text-sm font-medium">
-                                                        {t(step.timelineLabel)}
-                                                    </p>
-                                                    {config[step.id][step.status](
-                                                        application,
-                                                        step
-                                                    )}
-                                                </div>
-                                                <div className="whitespace-nowrap text-right text-gray-500 text-sm">
-                                                    <time dateTime={step.date}>
-                                                        {date({
-                                                            locale: locale,
-                                                            scheme: 'd MMMM y',
-                                                            value: step.date
-                                                        })}
-                                                    </time>
-                                                </div>
+                                            <div className="whitespace-nowrap text-right text-gray-500 text-sm">
+                                                <time dateTime={step.date}>
+                                                    {date({
+                                                        locale: locale,
+                                                        scheme: 'd MMMM y',
+                                                        value: step.date
+                                                    })}
+                                                </time>
                                             </div>
                                         </div>
                                     </div>
-                                );
-                            })}
+                                </div>
+                            );
+                        })}
                 </li>
             </ul>
         </div>
