e8f21af478247d61f4af5a4a029518640281ed86
Paul Cailly <pcailly@pm.me>
2021-05-29 08:01:27 +0200

    feat(app notifications): add notifications render

--- packages/ui/src/notifications/Notifications.tsx
+++ packages/ui/src/notifications/Notifications.tsx
@@ -1,6 +1,9 @@
-import { Notification } from '@applyfuture/models';
+import { Notification, SupportedLocale } from '@applyfuture/models';
+import { date, getAppNotificationById } from '@applyfuture/utils';
 import { faBell } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
+import useTranslation from 'next-translate/useTranslation';
 import React, { FC, useState } from 'react';
 
 import { OutsideAlerter } from './../outside-alerter/OutsideAlerter';
@@ -16,14 +19,29 @@ type Props = {
 export const Notifications: FC<Props> = (props) => {
     const { notifications } = props;
     const unseenNotifications = notifications?.filter((notification) => !notification.seen);
+    const { t } = useTranslation();
+    const router = useRouter();
+    const locale = router.locale as SupportedLocale;
 
     const [open, setOpen] = useState(false);
+    const [showOldNotifications, setShowOldNotifications] = useState(false);
 
     const handleToggle = () => setOpen((prev) => !prev);
 
     const handleClose = () => {
         setOpen(false);
+        setShowOldNotifications(false);
     };
+
+    const handleShowOldNotifications = () => setShowOldNotifications(true);
+
+    const handleClick = (link: string) => {
+        handleClose();
+        router.push(link);
+    };
+
+    const sortByDate = (a: Notification, b: Notification) =>
+        new Date(b.date).getTime() - new Date(a.date).getTime();
 
     const trigger = (
         <button className="flex items-center focus:outline-none" onClick={handleToggle}>
@@ -57,11 +75,71 @@ export const Notifications: FC<Props> = (props) => {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                         show={open}>
-                        <div className="absolute z-40 right-0 mt-2 truncate rounded-md shadow-lg origin-top-right">
+                        <div
+                            className="absolute z-40 right-0 mt-2 truncate rounded-md shadow-lg origin-top-right"
+                            style={{ maxHeight: '500px', width: '300px' }}>
                             <div className="py-1 bg-white rounded-md shadow-xs">
-                                {notifications.map((notification) => {
-                                    return <div key={notification.id}>{notification.id}</div>;
-                                })}
+                                {unseenNotifications.length === 0 && !showOldNotifications && (
+                                    <div className="px-6 py-5 bg-white">
+                                        <img
+                                            alt={t('application:no-new-notifications-illustration')}
+                                            src="/assets/images/application/no-new-notifications.svg"
+                                        />
+                                        <div className="mt-4 text-center text-gray-800 text-sm font-medium">
+                                            {t('application:no-new-notifications')}
+                                        </div>
+                                        {unseenNotifications.length > 0 && (
+                                            <button
+                                                className="mt-2 text-center text-gray-600 hover:text-indigo-500 underline text-sm font-normal"
+                                                onClick={handleShowOldNotifications}>
+                                                {t('application:show-old-notifications')}
+                                            </button>
+                                        )}
+                                    </div>
+                                )}
+                                {(unseenNotifications.length > 0 || showOldNotifications) &&
+                                    notifications.sort(sortByDate).map((notification, index) => {
+                                        const appNotification = getAppNotificationById(
+                                            notification.id
+                                        );
+
+                                        if (!appNotification) {
+                                            return null;
+                                        }
+
+                                        return (
+                                            <div key={index} className="cursor-pointer">
+                                                <button
+                                                    className="relative z-50 px-6 py-5 whitespace-normal hover:bg-gray-100 bg-white"
+                                                    onClick={() => {
+                                                        handleClick(notification.link);
+                                                    }}>
+                                                    <div className="text-gray-800 text-sm font-medium">
+                                                        {t(
+                                                            appNotification.title,
+                                                            JSON.parse(notification.variables)
+                                                        )}
+                                                    </div>
+                                                    <div className="mt-2 text-gray-600 text-sm font-normal">
+                                                        {t(
+                                                            appNotification.description,
+                                                            JSON.parse(notification.variables)
+                                                        )}
+                                                    </div>
+                                                    <div className="mt-2 text-gray-700 text-xs">
+                                                        {t('common:elapsed-time-ago', {
+                                                            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                                                            elapsedTime: date({
+                                                                locale: locale,
+                                                                method: 'DISTANCE',
+                                                                value: notification.date
+                                                            })!
+                                                        })}
+                                                    </div>
+                                                </button>
+                                            </div>
+                                        );
+                                    })}
                             </div>
                         </div>
                     </Transition>
