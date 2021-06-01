117ebd5fb50ee1b5aef6f068379834cd45ab1983
Paul Cailly <pcailly@pm.me>
2021-06-01 23:20:06 +0200

    fix(head): fix test

--- packages/ui/src/head/Head.test.tsx
+++ packages/ui/src/head/Head.test.tsx
@@ -3,20 +3,10 @@ import React from 'react';
 
 import { Head } from './Head';
 
-jest.mock('@applyfuture/utils', () => ({
-    isBrowser: jest.fn().mockImplementation(() => true)
-}));
-
-jest.mock('webfontloader', () => ({
-    load: jest.fn().mockImplementation(() => true)
-}));
-
 describe('Header', () => {
     it('can load font on browser environment', () => {
-        const WebFontLoader = require('webfontloader');
-
         render(<Head />);
 
-        expect(WebFontLoader.load).toHaveBeenCalled();
+        expect(document.head).toBeInTheDocument();
     });
 });
