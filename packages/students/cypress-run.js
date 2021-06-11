9ceec0e1cd101e8c662ca5de6c4dfc2786f2adae
Paul Cailly <pcailly@pm.me>
2021-06-12 01:17:21 +0200

    test(e2e): update github action

--- packages/students/cypress-run.js
+++ packages/students/cypress-run.js
@@ -2,7 +2,7 @@ const { v1 } = require('uuid');
 const execSync = require('child_process').execSync;
 
 execSync(
-  'cypress run --record --reporter mochawesome --reporter-options reportDir=cypress/report/mochawesome-report,overwrite=false,html=false,json=true,timestamp=mmddyyyy_HHMMss -e uuid=' +
+  'cypress run --record --key 7f72678d-329f-4ed2-892a-8fc9f04725b8 --reporter mochawesome --reporter-options reportDir=cypress/report/mochawesome-report,overwrite=false,html=false,json=true,timestamp=mmddyyyy_HHMMss -e uuid=' +
   v1(),
   { stdio: [0, 1, 2] }
 );
