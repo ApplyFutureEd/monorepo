d82467306e245a9bdb6165747c5eefa2209c1d90
Paul Cailly <pcailly@pm.me>
2021-06-14 23:10:25 +0200

    test(phone-input): fix test

--- packages/ui/src/phone-input/PhoneInput.test.tsx
+++ packages/ui/src/phone-input/PhoneInput.test.tsx
@@ -3,7 +3,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { PhoneInput } from './PhoneInput';
 
 describe('PhoneInput', () => {
-    const baseClasses = 'form-input block w-full text-sm leading-5 min-h-input';
+    const baseClasses = 'form-input flex w-full text-sm leading-5 min-h-input';
     const disabledClasses = 'bg-gray-100 cursor-not-allowed';
     const onErrorClasses =
         'placeholder-red-300 pr-10 text-red-900 border-red-300 focus:border-red-300 focus:shadow-outline-red';
