1350d3c514723400327d2fc52dc9f33806dab64d
Paul Cailly <pcailly@pm.me>
2021-07-08 06:58:34 +0200

    quest(monorepo): update cz-config.js
    Resolves Quest #239

--- .cz-config.js
+++ .cz-config.js
@@ -30,13 +30,10 @@ module.exports = {
     // override the messages, defaults are as follows
     messages: {
         type: "Select the type of change that you're committing:",
-        scope: '\nChoose the scope of this change (optional):',
         // used if allowCustomScopes is true
-        customScope: '\nChoose the scope of this change (optional)',
+        customScope: '\nSelect the scope of this change (optional)',
         subject: 'Write a short, imperative tense description of the change:\n',
-        body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-        breaking: 'List any BREAKING CHANGES (optional):\n',
-        footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+        footer: 'Quest resolved by this change (optional). E.g.: #31, #34:\n',
         confirmCommit: 'Are you sure you want to proceed with the commit above?'
     },
 
