5d5d3db2feef8a06f9bcb90d470eb880cee2a827
Paul Cailly <pcailly@pm.me>
2021-07-14 22:26:36 +0200

    ⚔️ quest(monorepo): upgrade next-translate

--- packages/students/next.config.js
+++ packages/students/next.config.js
@@ -1,44 +1,46 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const withPWA = require('next-pwa');
-const { locales, defaultLocale } = require('./i18n');
-
-const COMMIT_SHA = process.env.VERCEL_GITHUB_COMMIT_SHA;
-const basePath = '';
-
 const withTM = require('next-transpile-modules')([
     '@applyfuture/graphql',
     '@applyfuture/ui',
     '@applyfuture/utils',
     '@applyfuture/models'
 ]);
+const withPWA = require('next-pwa');
+const withNextTranslate = require('next-translate');
+const { locales, defaultLocale } = require('./i18n');
+
+const COMMIT_SHA = process.env.VERCEL_GITHUB_COMMIT_SHA;
+const basePath = '';
 
 module.exports = withTM(
-    withPWA({
-        basePath,
-        env: {
-            ASSETS_CDN_URL: 'https://ik.imagekit.io/applyfuture',
-            NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA
-        },
-        i18n: { defaultLocale, localeDetection: true, locales },
-        images: {
-            deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-            domains: ['ik.imagekit.io'],
-            imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-            loader: 'default',
-            path: '/_next/image'
-        },
-        poweredByHeader: false,
-        pwa: {
-            dest: 'public',
-            disable: process.env.NODE_ENV === 'development'
-        },
-        webpack: (config) => {
-            config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
-            config.resolve.alias['@pages'] = path.join(__dirname, 'src/pages_');
-            config.resolve.alias['@styles'] = path.join(__dirname, 'src/styles');
+    withPWA(
+        withNextTranslate({
+            basePath,
+            env: {
+                ASSETS_CDN_URL: 'https://ik.imagekit.io/applyfuture',
+                NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA
+            },
+            i18n: { defaultLocale, localeDetection: true, locales },
+            images: {
+                deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+                domains: ['ik.imagekit.io'],
+                imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+                loader: 'default',
+                path: '/_next/image'
+            },
+            poweredByHeader: false,
+            pwa: {
+                dest: 'public',
+                disable: process.env.NODE_ENV === 'development'
+            },
+            webpack: (config) => {
+                config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
+                config.resolve.alias['@pages'] = path.join(__dirname, 'src/pages_');
+                config.resolve.alias['@styles'] = path.join(__dirname, 'src/styles');
 
-            return config;
-        }
-    })
+                return config;
+            }
+        })
+    )
 );
