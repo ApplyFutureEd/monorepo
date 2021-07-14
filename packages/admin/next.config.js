987feb0d600a7e13daa95daed59e5d738372d692
Paul Cailly <pcailly@pm.me>
2021-07-14 20:12:52 +0200

    💥 bounty-hunt(monorepo): remove sentry

--- packages/admin/next.config.js
+++ packages/admin/next.config.js
@@ -3,8 +3,6 @@ const path = require('path');
 const bundleAnalyzer = require('@next/bundle-analyzer');
 const withPWA = require('next-pwa');
 const nextSourceMaps = require('@zeit/next-source-maps');
-const SentryWebpackPlugin = require('@sentry/webpack-plugin');
-
 const withSourceMaps = nextSourceMaps();
 const withBundleAnalyzer = bundleAnalyzer({
     enabled: process.env.ANALYZE_BUNDLE === 'true'
@@ -12,21 +10,7 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 const { locales, defaultLocale } = require('./i18n');
 
-const {
-    NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-    SENTRY_ORG,
-    SENTRY_PROJECT,
-    SENTRY_AUTH_TOKEN,
-    NODE_ENV,
-    VERCEL_GITHUB_COMMIT_SHA,
-    VERCEL_GITLAB_COMMIT_SHA,
-    VERCEL_BITBUCKET_COMMIT_SHA
-} = process.env;
-
-const COMMIT_SHA =
-    VERCEL_GITHUB_COMMIT_SHA || VERCEL_GITLAB_COMMIT_SHA || VERCEL_BITBUCKET_COMMIT_SHA;
-
-process.env.SENTRY_DSN = SENTRY_DSN;
+const COMMIT_SHA = process.env.VERCEL_GITHUB_COMMIT_SHA;
 const basePath = '';
 
 const withTM = require('next-transpile-modules')([
@@ -63,9 +47,6 @@ module.exports = withTM(
                         fs: 'empty'
                     };
 
-                    if (!options.isServer) {
-                        config.resolve.alias['@sentry/node'] = '@sentry/browser';
-                    }
                     config.plugins.push(
                         new options.webpack.DefinePlugin({
                             'process.env.NEXT_IS_SERVER': JSON.stringify(
@@ -73,24 +54,6 @@ module.exports = withTM(
                             )
                         })
                     );
-                    if (
-                        SENTRY_DSN &&
-                        SENTRY_ORG &&
-                        SENTRY_PROJECT &&
-                        SENTRY_AUTH_TOKEN &&
-                        COMMIT_SHA &&
-                        NODE_ENV === 'production'
-                    ) {
-                        config.plugins.push(
-                            new SentryWebpackPlugin({
-                                ignore: ['node_modules'],
-                                include: '.next',
-                                release: COMMIT_SHA,
-                                stripPrefix: ['webpack://_N_E/'],
-                                urlPrefix: `~${basePath}/_next`
-                            })
-                        );
-                    }
 
                     config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
                     config.resolve.alias['@pages'] = path.join(__dirname, 'src/pages_');
