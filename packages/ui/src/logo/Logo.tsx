cc5a407065f97a80db1789fa665b9d16903557f8
Paul Cailly <pcailly@pm.me>
2021-07-15 02:54:31 +0200

    💥 bounty-hunt(monorepo): remove blur on logo

--- packages/ui/src/logo/Logo.tsx
+++ packages/ui/src/logo/Logo.tsx
@@ -4,5 +4,5 @@ import { FC } from 'react';
 import logo from './logo.png';
 
 export const Logo: FC = () => {
-    return <Image alt="logo" height="32" placeholder="blur" src={logo} width="146" />;
+    return <Image alt="logo" height="32" src={logo} width="146" />;
 };
