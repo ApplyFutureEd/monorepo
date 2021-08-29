7744bf4724d2f257ed4d2da1c35ee32d01d36e65
Paul Cailly <pcailly@pm.me>
2021-08-29 14:52:13 +0200

    💥 bounty-hunt(students): fix lead modal test

--- packages/students/src/components/landing/lead-modal/LeadModal.test.tsx
+++ packages/students/src/components/landing/lead-modal/LeadModal.test.tsx
@@ -1,23 +1,7 @@
 import LeadModal from '@components/landing/lead-modal/LeadModal';
 import { render, screen } from '@testing-library/react';
-import { useState as useStateMock } from 'react';
-
-/* jest.mock('react', () => ({
-    ...(jest.requireActual('react') as Record<string, unknown>),
-    useState: jest.fn()
-})); */
 
 describe('LeadModal', () => {
-    const setOpenModal = jest.fn();
-
-    beforeEach(() => {
-        (useStateMock as jest.Mock).mockImplementation(() => [true, setOpenModal]);
-    });
-
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
     it('can render without crashing', () => {
         render(
             <div>
