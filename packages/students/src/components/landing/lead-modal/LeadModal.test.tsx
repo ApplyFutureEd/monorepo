519c2b243210d9b2b4280b8ed134d5d6b5ecaf26
Paul Cailly <pcailly@pm.me>
2021-08-29 15:15:09 +0200

    💥 bounty-hunt(students): fix LeadModal test

--- packages/students/src/components/landing/lead-modal/LeadModal.test.tsx
+++ packages/students/src/components/landing/lead-modal/LeadModal.test.tsx
@@ -1,7 +1,12 @@
 import LeadModal from '@components/landing/lead-modal/LeadModal';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import Cookies from 'js-cookie';
+
+jest.mock('js-cookie');
 
 describe('LeadModal', () => {
+    jest.useFakeTimers();
+
     it('can render without crashing', () => {
         render(
             <div>
@@ -9,8 +14,46 @@ describe('LeadModal', () => {
             </div>
         );
 
+        jest.advanceTimersByTime(5000);
+
         const heading = screen.getByRole('heading');
 
         expect(heading).toBeInTheDocument();
+    });
+
+    it('can call onClose callback function when the close icon is clicked', async () => {
+        render(
+            <div>
+                <LeadModal />
+            </div>
+        );
+
+        jest.advanceTimersByTime(5000);
+
+        const closeIcon = screen.getByRole('img', { hidden: true });
+
+        await waitFor(() => {
+            fireEvent.click(closeIcon);
+        });
+
+        const heading = screen.queryByRole('heading');
+
+        expect(heading).not.toBeInTheDocument();
+    });
+
+    it('does not open modal if cookie exist', () => {
+        Cookies.get = jest.fn().mockImplementation(() => true);
+
+        render(
+            <div>
+                <LeadModal />
+            </div>
+        );
+
+        jest.advanceTimersByTime(5000);
+
+        const heading = screen.queryByRole('heading');
+
+        expect(heading).not.toBeInTheDocument();
     });
 });
