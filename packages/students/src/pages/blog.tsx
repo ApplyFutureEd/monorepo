18c16606eb5e89624bf150b406db4daa4e7c4313
Arthur Sallé <arthursalle.pro@gmail.com>
2021-09-29 17:24:27 +0200

    ⚔️ quest(students): remove list component

--- packages/students/src/pages/blog.tsx
+++ packages/students/src/pages/blog.tsx
@@ -1,4 +1,5 @@
-import ArticleList from '@components/blog/article-list/ArticleList';
+import posts from '@assets/posts/posts';
+import Article from '@components/blog/article/Article';
 import Hero from '@components/blog/hero/Hero';
 import LandingLayout from '@components/layouts/landing-layout/LandingLayout';
 import { FC } from 'react';
@@ -9,7 +10,23 @@ const Blog: FC = () => {
             <div className="bg-white overflow-hidden">
                 <div className="relative mx-auto py-16 max-w-7xl sm:px-6 lg:px-8">
                     <Hero />
-                    <ArticleList />
+                    <div className="grid gap-12 mx-4 sm:grid-cols-2 sm:mx-auto lg:grid-cols-3 lg:max-w-none">
+                        {posts.map((post: any) => {
+                            return (
+                                <div key={post.id}>
+                                    <Article
+                                        author={post.author}
+                                        description={post.description}
+                                        image={post.image}
+                                        publicationDate={post.publicationDate}
+                                        readingTime={post.readingTime}
+                                        tag={post.tag}
+                                        title={post.title}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </LandingLayout>
