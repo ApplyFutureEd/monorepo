94a3c82626c11d6df4597259c4cae5e197e867b0
clepirault <clepirault@gmail.com>
2021-10-06 13:34:40 +0200

    ⚔️ quest(i18n): add statistics test file

---
+++ packages/i18n/src/components/statistics/Statistics.test.tsx
@@ -0,0 +1,42 @@
+import Statistics from '@components/statistics/Statistics';
+import { Translation, Values } from '@pages/index';
+import { render, screen } from '@testing-library/react';
+
+describe('Statistics', () => {
+    const isLoading = false;
+    const translations = [
+        {
+            key: 'bonjour',
+            namespace: 'namespace',
+            values: { en: 'hello', fr: 'bonjour', zh: '你好' } as Values
+        },
+        {
+            key: 'au revoir',
+            namespace: 'namespace',
+            values: { en: 'goodbye', fr: 'au revoir', zh: '' } as Values
+        }
+    ] as Translation[];
+
+    it('can display statistics label without crashing', () => {
+        render(<Statistics isLoading={isLoading} translations={translations} />);
+        const label = 'Translations';
+        const statistics = screen.getByText(label).closest('dt');
+        expect(statistics).toBeInTheDocument();
+    });
+
+    it('can display translations statistics without crashing', () => {
+        render(<Statistics isLoading={isLoading} translations={translations} />);
+        const total = '2';
+        const complete = '1';
+        const uncomplete = '1';
+        const completion = '50 %';
+        const totalStatistics = screen.getAllByText(total);
+        expect(totalStatistics[0]).toBeInTheDocument();
+        const completeStatistics = screen.getAllByText(complete);
+        expect(completeStatistics[0]).toBeInTheDocument();
+        const uncompleteStatistics = screen.getAllByText(uncomplete);
+        expect(uncompleteStatistics[0]).toBeInTheDocument();
+        const completionStatistics = screen.getAllByText(completion);
+        expect(completionStatistics[0]).toBeInTheDocument();
+    });
+});
