6db4642693ca23b538ffb6184034e5f4146b3946
pcailly <pcailly@pm.me>
2021-10-11 21:51:15 +0200

    🌍 i18n(monorepo): update translations

--- locales.sh
+++ locales.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf ./locales
 rm -rf ./packages/students/locales
 rm -rf ./packages/admin/locales
 rm -rf ./packages/i18n/locales
@@ -9,3 +8,4 @@ aws s3 cp s3://applyfuture-students-content162403-dev/public/i18n ./locales --re
 cp -R ./locales ./packages/students/locales
 cp -R ./locales ./packages/admin/locales
 cp -R ./locales ./packages/i18n/locales
+rm -rf ./locales
