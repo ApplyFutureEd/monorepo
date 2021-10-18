14e58e858fc0c6700171902d6ae475a6e07a5ff9
Arthur Sallé <arthursalle.pro@gmail.com>
2021-10-18 21:54:29 +0200

    ⚔️ quest(students): add formik to newsletter section

--- packages/students/src/components/blog/newsletter/Newsletter.tsx
+++ packages/students/src/components/blog/newsletter/Newsletter.tsx
@@ -1,13 +1,31 @@
-import { Button } from '@applyfuture/ui';
+import { Button, Input } from '@applyfuture/ui';
+import { Field, FieldProps, Form, Formik } from 'formik';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 const Newsletter: FC = () => {
+    const [email, setEmail] = useState('');
+
+    type initialValues = {
+        email: string;
+    };
+    const initialValues: initialValues = {
+        email: ''
+    };
+
+    const onSubmit = () => {
+        setEmail(email);
+        console.log('yo' + email);
+    };
+
     return (
-        <div className="bg-white">
+        <Formik
+            className="items-center bg-white sm:flex"
+            initialValues={initialValues}
+            onSubmit={onSubmit}>
             <div className="mx-auto px-4 py-24 max-w-7xl sm:px-6 lg:flex lg:items-center lg:px-8 lg:py-32">
                 <div className="lg:flex-1 lg:w-0">
-                    <h2 className="text-gray-800 text-3xl font-bold sm:text-4xl">
+                    <h2 className="text-gray-900 text-3xl font-extrabold sm:text-4xl">
                         Sign up for our newsletter
                     </h2>
                     <p className="mt-3 max-w-3xl text-gray-500 text-lg">
@@ -16,25 +34,23 @@ const Newsletter: FC = () => {
                     </p>
                 </div>
                 <div className="mt-8 lg:ml-8 lg:mt-0">
-                    <form className="items-center sm:flex">
-                        <label className="sr-only" htmlFor="email-address">
+                    <div className="items-start sm:flex">
+                        <label className="sr-only" htmlFor="email">
                             Email address
                         </label>
-                        <input
-                            required
-                            autoComplete="email"
-                            className="placeholder-gray-400 px-5 py-3 w-full border border-gray-300 focus:border-indigo-500 rounded-md shadow-sm focus:ring-1 focus:ring-indigo-500 sm:max-w-xs"
-                            id="email-address"
-                            name="email-address"
-                            placeholder="Enter your email"
-                            type="email"
-                        />
-                        <div className="mt-3 sm:flex-shrink-0 sm:ml-3 sm:mt-0">
-                            <Button type="submit" variant="primary">
+                        <Form className="w-full">
+                            <Field id="email" name="email">
+                                {(fieldProps: FieldProps) => (
+                                    <Input placeholder="Enter your email" {...fieldProps} />
+                                )}
+                            </Field>
+                        </Form>
+                        <div className="mt-3 rounded-md sm:flex-shrink-0 sm:ml-3 sm:mt-0">
+                            <Button type="submit" variant="primary" onClick={onSubmit}>
                                 Notify me
                             </Button>
                         </div>
-                    </form>
+                    </div>
                     <p className="mt-3 text-gray-500 text-sm">
                         We care about the protection of your data. Read our
                         <Link href="/privacy-policy">
@@ -43,7 +59,7 @@ const Newsletter: FC = () => {
                     </p>
                 </div>
             </div>
-        </div>
+        </Formik>
     );
 };
 
